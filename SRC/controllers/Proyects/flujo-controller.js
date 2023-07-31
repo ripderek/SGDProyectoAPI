e6f81acf09c64ace3816237ad062027fdc9aa857
Raul <castillosteven09@outlook.es>
2023-07-31 11:05:49 -0500

    Flujo controllers

--- SRC/controllers/Proyects/flujo-controller.js
+++ SRC/controllers/Proyects/flujo-controller.js
@@ -3,8 +3,8 @@ const pool = require('../../db');
 
 const crear_nivel = async (req, res, next) => {
     try {
-        const { p_titulo, p_descripcion } = req.body;
-        const users = await pool.query('call Crear_Nivel($1,$2)', [p_titulo, p_descripcion]);
+        const { p_titulo, p_descripcion, p_cardinalidad } = req.body;
+        const users = await pool.query('call Crear_Nivel($1,$2,$3)', [p_titulo, p_descripcion, p_cardinalidad]);
         console.log(users);
         return res.status(200).json({ message: "Se creo el nivel" });
     } catch (error) {
@@ -12,7 +12,17 @@ const crear_nivel = async (req, res, next) => {
         return res.status(404).json({ message: error.message });
     }
 }
+const ver_niveles = async (req, res, next) => {
+    try {
+        const users = await pool.query('select * from ver_niveles()');
+        console.log(users);
+        return res.status(200).json(users.rows);
+    } catch (error) {
+        return res.status(404).json({ message: error.message });
+    }
+}
 
 module.exports = {
-    crear_nivel
+    crear_nivel,
+    ver_niveles
 };
--- SRC/routes/flujo-routes.js
+++ SRC/routes/flujo-routes.js
@@ -2,7 +2,8 @@ const { Router } = require('express');
 const router = Router();
 
 
-const { crear_nivel } = require("../controllers/Proyects/flujo-controller");
+const { crear_nivel, ver_niveles } = require("../controllers/Proyects/flujo-controller");
 router.post("/CrearNivel", crear_nivel);
+router.get("/Ver_niveles", ver_niveles);
 
 module.exports = router;
