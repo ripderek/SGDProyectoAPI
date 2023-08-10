63fa02074d0f8a56a2cd10e4c2f9f09e584e0183
Raul <castillosteven09@outlook.es>
2023-08-09 21:24:46 -0500

    Subir Level

--- SRC/routes/public-routes.js
+++ SRC/routes/public-routes.js
@@ -4,9 +4,11 @@ const router = Router();
 
 //importar el modulo empresa controller 
 const { datos_Empresa, imagen_empresa } = require('../controllers/Empresa/empresa-controller');
+const { proyectos_publicados } = require('../controllers/Proyects/proyects-controller');
 
 
 router.get('/Datos_Empresa', datos_Empresa);
 router.get('/Imagen_Empresa', imagen_empresa);
+router.get('/Proyectos', proyectos_publicados);
 
 module.exports = router;
